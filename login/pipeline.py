b917c956555e50e87d5f62be3f99ef41224b718f
Krystian <matematyk45@o2.pl>
2025-03-13 01:16:29 +0100

    debugging

--- login/pipeline.py
+++ login/pipeline.py
@@ -12,8 +12,6 @@ def append_UID_to_database_fields(strategy, details, request, backend, user=None
     return
 
 def check_wtf_is_wrong(strategy, uid, details, request, backend, user=None, *args, **kwargs):
-    """  print(strategy)
-    print(backend)
     print("REQUEST  ", request)
     print("DETAILS ", details)
     print("user  ", user)
@@ -21,8 +19,4 @@ def check_wtf_is_wrong(strategy, uid, details, request, backend, user=None, *arg
     print("kwargs  ", kwargs)
     print(backend.name, uid)
     print(backend.strategy.storage.user.get_social_auth(backend.name, uid))
-    print(backend.strategy.storage.user)
-    print(backend.strategy.storage)
-    print(backend.strategy)"""
-
     return
